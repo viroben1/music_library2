5de06a6f2717c612145118490e4553d008e6a232
benjamin farmer <viroben2001@gmail.com>
2023-01-10 15:21:53 -0800

    added code in appjs and galleryitems

--- src/componets/SearchBar.js
+++ src/componets/SearchBar.js
@@ -1,15 +1,21 @@
                 
         import { useState } from 'react'
-        function SearchBar(props) {
-            let [searchTerm, setSearchTerm] = useState('')
 
+        function SearchBar(props){
+            let [searchTerm, setSearchTerm] = useState('')
+        
             return (
-                <form>
-                    <input type="text" placeholder="Enter a search term here" />
+                <form onSubmit={(e) => props.handleSearch(e, searchTerm)}>
+        
+                    <input type="text" placeholder="Enter a search term here"
+                    onChange={(e) => setSearchTerm(e.target.value)} />
+        
                     <input type="submit" />
+        
                 </form>
             )
         }
+        
 
         export default SearchBar
     
