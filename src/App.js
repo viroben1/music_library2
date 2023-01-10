836dbaf323a63cac2cb907437c24a00d323eca6d
benjamin farmer <viroben2001@gmail.com>
2023-01-10 15:49:09 -0800

    added more context

--- src/App.js
+++ src/App.js
@@ -1,38 +1,45 @@
 import { DataContext } from './context/DataContext'
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
+import { SearchContext } from './context/SearchContext'
 import Gallery from './components/Gallery'
 import SearchBar from './components/SeachBar'
 const App = () => {
   let [search, setSearch] = useState('')
   let [message, setMessage] = useState('Search for Music!')
   let [data, setData] = useState([])
-
+  let searchInput = useRef('')
   const API_URL = 'https://itunes.apple.com/search?term='
 
   useEffect(() => {
       if(search) {
-          const fetchData = async () => {
-              document.title = `${search} music`
-              const response = await fetch(API_URL + search)
-              const resData = await response.json()
-              if (resData.results.length > 0) {
-                  return setData(resData.results)
-              } else {
-                  return setMessage('Not Found.')
-              }
-          }
-          fetchData()
-      }
+          
+     }
   }, [search])
 
   const handleSearch = (e, term) => {
-      e.preventDefault()
-      setSearch(term)
-  }
+    e.preventDefault()
+    const fetchData = async () => {
+        document.title = `${term} Music`
+        const response = await fetch(API_URL + term)
+        const resData = await response.json()
+        if (resData.results.length > 0) {
+            return setData(resData.results)
+        } else {
+            return setMessage('Not Found.')
+        }
+    }
+    fetchData()
+}
+
 
   return (
       <div>
-          <SearchBar handleSearch={handleSearch} />
+          <SearchContext.Provider value={{
+                term: searchInput,
+                handleSearch: handleSearch
+            }}>
+                <SearchBar />
+            </SearchContext.Provider>
           {message}
           <DataContext.Provider value={data} >
                 <Gallery />
@@ -42,4 +49,3 @@ const App = () => {
 }
 
 export default App
-
--- src/componets/SearchBar.js
+++ src/componets/SearchBar.js
@@ -1,21 +1,20 @@
                 
-        import { useState } from 'react'
-
-        function SearchBar(props){
-            let [searchTerm, setSearchTerm] = useState('')
+        import { useContext } from 'react'
+        import { SearchContext } from '../context/SearchContext'
+        
+        function SearchBar() {
+            const {term, handleSearch} = useContext(SearchContext)
         
             return (
-                <form onSubmit={(e) => props.handleSearch(e, searchTerm)}>
-        
-                    <input type="text" placeholder="Enter a search term here"
-                    onChange={(e) => setSearchTerm(e.target.value)} />
-        
-                    <input type="submit" />
-        
+                <form>
+                    <input ref={term} type="text" placeholder="Search Here" />
+                    <button onClick={(e) => handleSearch(e, term.current.value)}>Submit</button>
                 </form>
             )
         }
         
+        
 
         export default SearchBar
-    
+        
+        
